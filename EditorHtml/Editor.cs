b66b316b65c004d5caf1b64054e49dc0c0a53950
Murilo Barbosa <Mubarbosa006@gmail.com>
2023-01-30 20:37:51 -0300

    Aula c# EditorHtml

---
+++ EditorHtml/Editor.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+
+namespace EditorHtml{
+    public static class Editor{
+        
+        public static void Show(){
+            Console.Clear();
+            Console.BackgroundColor= ConsoleColor.Blue;
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.Clear();
+            Console.SetCursorPosition(8,0);
+            Console.WriteLine("MODO EDITOR"); 
+            Console.WriteLine(" ------------------");
+            Start();
+        }
+        
+        public static void Start()
+        {
+            var file = new StringBuilder();
+            Console.WriteLine("");
+            do {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            } while(Console.ReadKey().Key != ConsoleKey.Escape);
+            
+            Console.WriteLine("f");
+            Console.WriteLine(" Deseja salvar o arquivo? Sim-S ou Não-N? ");
+
+            char resp = char.Parse(Console.ReadLine());
+            if(resp== 's')
+            {  
+                Console.WriteLine("Aquivo salvo com sucesso");
+                Thread.Sleep(2500);
+                Menu.Show();
+            }
+            if(resp== 'n')
+            Menu.Show();
+        }
+        
+    }
+}
